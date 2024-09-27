7833596506f42186c4b8d4a3efb326a65deb2998
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-09-27 10:31:33 +0500

    tipsy sketching

---
+++ app.js
@@ -0,0 +1,8 @@
+var mainLogIn = document.getElementById("singin")
+mainLogIn&&mainLogIn.addEventListener('click',()=>{
+    location.href="login.html"
+})
+var mainSignUp=document.getElementById("singup")
+mainSignUp&&mainSignUp.addEventListener('click',()=>{
+    location.href="signup.html"
+})
